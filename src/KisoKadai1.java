3d52af20a83fc2348436af360eb41b341b15ee86
Sakai Yoshiaki <yoshi_zega@icloud.com>
2016-01-26 19:38:12 +0900

    修正三回目

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -1,23 +1,29 @@
+public class KisoKadai1 {
 
-public class KisoKadai1{
+	/**
+	 * @param args
+	 */
 
-	  public static void main(String args[])
-	  {
-		    System.out.println("九九の表");
+	public static void main(String[] args){
+		  System.out.println("九九の表");
 
 	    	System.out.println("Xを入力してください。");
 		int X= new java.util.Scanner(System.in ).nextInt();
 
 		System.out.println("Yを入力してください。");
 		int Y= new java.util.Scanner(System.in ).nextInt();
-		    // 九九の結果を表示。
-		    for( int i=1; i<=X; i++ ){
-		      for( int j=1; j<=Y; j++ ){
-		    	  System.out.print (i*j+" ");
-		      }
-		      	  System.out.println();
-		    }
-	  }
+		for(int i=1;i<=X;i++){
+			for(int j=1;j<=Y;j++){
+				System.out.print((i*j)>=10 ? " "+(i*j) : "  "+(i*j));
+			}
+		System.out.println();
+		}
+
+
+	}
 }
+
+
+
 
 
