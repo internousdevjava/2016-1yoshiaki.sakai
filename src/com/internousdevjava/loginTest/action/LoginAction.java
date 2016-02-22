5a4b4e5238febfcd105a822f4c7187f0837f600e
Sakai Yoshiaki <yoshi_zega@icloud.com>
2016-02-22 09:54:35 +0900

    金曜提出物

---
+++ src/com/internousdevjava/loginTest/action/LoginAction.java
@@ -0,0 +1,68 @@
+package com.internousdevjava.loginTest.action;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.internousdevjava.loginTest.dao.LoginDAO;
+import com.internousdevjava.loginTest.dto.LoginDTO;
+import com.opensymphony.xwork2.ActionSupport;
+/**
+ * @author internous
+ *
+ */
+public class LoginAction extends ActionSupport implements SessionAware{
+	/**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    public String id;
+	public String password;
+	private Map<String,Object>session;
+
+	public String execute(){
+
+		LoginDAO dao = new LoginDAO(); //LoginDAOクラスを作成すると波線が消える
+
+		boolean res = dao.select(id,password);
+
+		if(!res){
+			return ERROR;
+		}
+		 LoginDTO dto = new LoginDTO(id, password);
+
+	        session.put("id", dto.getId());
+
+		session.put("id", id );
+
+		session.put("name_key", password );
+
+
+		return SUCCESS;
+	}
+
+	public String getId(){
+		return id;
+	}
+
+	public void setId(String id){
+		this.id = id;
+	}
+
+	public String getPass(){
+		return password;
+	}
+
+	public void setPass(String password){
+		this.password = password;
+	}
+
+	public Map<String, Object> getSession(){
+		return session;
+	}
+
+	public void setSession(Map<String, Object> session){
+		this.session = session;
+	}
+
+}
